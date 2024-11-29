079b1416f24b0adbd04700c7cabcacacc200fa15
Jan.Bolliger <jan.bolliger@stud.abbts.ch>
2024-11-29 18:59:16 +0100

    ToDos aus Code entfernt

--- ThemaGit/RollTheDice/src/main/kotlin/RollTheDice.kt
+++ ThemaGit/RollTheDice/src/main/kotlin/RollTheDice.kt
@@ -29,11 +29,6 @@ while (true) {
     val playerScore = die.nextInt(1..6)
     val computerScore = die.nextInt(1..6)
 
-    // Todo: Den Fehler im Programm finden und beheben
-    // Todo: Den Spieler nach dem Namen fragen
-    // Todo: Solange spielen bis Spieler abbricht
-    // Todo: Ausgeben, wer mehr Runden gewonnen hat
-
     println("$playerName würfelt: $playerScore  --  Computer würfelt: $computerScore")
     when {
         playerScore > computerScore -> {println("$playerName gewinnt")
@@ -46,11 +41,11 @@ while (true) {
     println("Nochmal spielen? (j/n)")
     if (readln() != "j") {
         println("Danke fürs Spiel! ")
-        println("")
+        println("............................................")
         println("$playerName hat $playerCount Runden gewonnen")
         println("der Computer hat $computerCount Runden gewonnen")
         println ("und es gab $unentschieden Unentschieden.")
-        println("")
+        println("............................................")
         when{
             playerCount > computerCount -> println("$playerName hat öfter gewonnen!")
             playerCount < computerCount -> println("Der Computer hat öfter gewonnen!")
